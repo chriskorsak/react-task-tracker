952962c1ed3a49831f4cce341287df03f9c9a02b
chriskorsak <chris@chriskorsak.com>
2021-12-07 22:37:14 -0800

    project setup using npm, add css styles to App.css, add Button, Header, and Tasks components, remove unessasary default files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 5:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Haircut Appointment',
+      day: 'Feb 12th at 9:00am',
+      reminder: false
+    }
+  ])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* example of props, you can pass this title prop to the header component */}
+      <Header title='hello dude' />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Button from './Button'
+
+const Header = (props) => {
+  const onClick = () => {
+    console.log('clicky click click!')
+  }
+
+  return (
+    <header className='header'>
+      <h1>Task {props.title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
+  )
+}
+
+export default Header
